1c85452ff9f59b748c7a281d549c2eebae9dd6e9
Georgi Naumov <gonaumov@gmail.com>
2021-05-08 04:51:10 +0300

    Deployment

---
+++ docs/precache-manifest.a0fdc0fb8fd440fadcbef2f46e3b8ce0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5568d39798aecfc94222",
+    "url": "/personalnumber/css/app.23ec98bd.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/personalnumber/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "2dfb2314f23b5e0e4c7237320ea4c95a",
+    "url": "/personalnumber/index.html"
+  },
+  {
+    "revision": "c11ab54dd7fcf328c326",
+    "url": "/personalnumber/js/about.b2306e26.js"
+  },
+  {
+    "revision": "5568d39798aecfc94222",
+    "url": "/personalnumber/js/app.1681d3a8.js"
+  },
+  {
+    "revision": "4634f51bc7e02310a1ce",
+    "url": "/personalnumber/js/chunk-vendors.2e15efec.js"
+  },
+  {
+    "revision": "1d96ce058bb2dd8e0692ba5840cd84ac",
+    "url": "/personalnumber/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/personalnumber/robots.txt"
+  }
+]);
